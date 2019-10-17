c23d4239a2259bef7969d7f3d4f10d4d1917f24c
Surya Kiran Suresh <skiran13@gmail.com>
2019-10-17 13:26:08 +0530

    Changes

--- app.py
+++ app.py
@@ -20,51 +20,58 @@ def home():
     return render_template('home.html')
 
 
+def preprocess(df):
+        # Tokenize the words
+    lines = df.message
+    tokenize = []
+    for word in lines:
+        tokenize.append(word_tokenize(word))
+    df.insert(1, "tokenized", lines)
+
+    # removed '.' and ',' completely
+    clean = [[word for word in lines if word != ',' if word != '.']
+             for lines in df['tokenized']]
+    df.insert(2, "clean", clean)
+
+    # Stem the words
+    porter_stemmer = PorterStemmer()
+    # Stem the words
+    stemmed_words = [[porter_stemmer.stem(
+        word=word) for word in lines] for lines in df['tokenized']]
+    df.insert(3, "stemmed", stemmed_words)
+
+    # Lemmatize the words
+    # if not installed
+    # nltk.download('wordnet')
+    lemmatizer = WordNetLemmatizer()
+    lemmatized = [[lemmatizer.lemmatize(
+        word=word, pos='v') for word in lines] for lines in df['stemmed']]
+    df.insert(4, "lemmatized", lemmatized)
+
+    # stop words removal
+    stop_words = set(stopwords.words('english'))
+    stop_remove = [[lemmatizer.lemmatize(
+        word=word, pos='v') for word in lines if word not in stop_words] for lines in df['stemmed']]
+    df.insert(5, "rem_stop", stop_remove)
+    df['rem_stop'] = df['rem_stop'].apply(', '.join)
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
     df = pd.read_csv("spam.csv", encoding="latin-1")
     df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
     # Features and Labels
     df['label'] = df['class'].map({'ham': 0, 'spam': 1})
-    
-    #Lower casing
+
+    # Lower casing
     df['message'] = df['message'].str.lower()
-    #Nltk for the first time. Run this
-    #nltk.download()
-    
-    #Tokenize the words
-    lines = df.message
-    tokenize = []
-    for word in lines:
-        tokenize.append(word_tokenize(word))
-    df.insert(1,"tokenized",lines)
-    
-    #removed '.' and ',' completely
-    clean = [[word for word in lines if word != ',' if word !='.'] for lines in df['tokenized']]
-    df.insert(2,"clean",clean)
-    
-    #Stem the words
-    porter_stemmer = PorterStemmer()
-    #Stem the words
-    stemmed_words = [[porter_stemmer.stem(word = word) for word in lines] for lines in df['tokenized']]
-    df.insert(3,"stemmed",stemmed_words)
-    
-    #Lemmatize the words
-    #if not installed
-    #nltk.download('wordnet') 
-    lemmatizer = WordNetLemmatizer()
-    lemmatized = [[lemmatizer.lemmatize(word=word,pos='v') for word in lines] for lines in df['stemmed']]
-    df.insert(4,"lemmatized",lemmatized)
-    
-    #stop words removal
-    stop_words = set(stopwords.words('english'))
-    stop_remove = [[lemmatizer.lemmatize(word=word,pos='v') for word in lines if word not in stop_words] for lines in df['stemmed']]
-    df.insert(5,"rem_stop",stop_remove)
-    df['rem_stop'] = df['rem_stop'].apply(', '.join)
-    X = df['tokenized']
+    # Nltk for the first time. Run this
+    # nltk.download()
+
+    X = df['message']
     y = df['label']
 
-    #Modelling
+    # Modelling
     # Extract Feature With CountVectorizer
     cv = CountVectorizer()
     X = cv.fit_transform(X)  # Fit the Data
@@ -76,26 +83,24 @@ def predict():
     from sklearn.pipeline import Pipeline
     from sklearn.feature_extraction.text import TfidfTransformer
 
+    clf = MultinomialNB()
+    clf.fit(X_train, y_train)
+    clf.score(X_test, y_test)
 
-    #clf = MultinomialNB()
-    #clf.fit(X_train, y_train)
-    #clf.score(X_test, y_test)
-
-    nb = Pipeline([('tfidf', TfidfTransformer()),
-               ('clf', MultinomialNB()),
-              ])
-    nb.fit(X_train, y_train)
+    # nb = Pipeline([('tfidf', TfidfTransformer()),
+    #               ('clf', MultinomialNB()), ])
+    #nb.fit(X_train, y_train)
 
     # Alternative Usage of Saved Model
-    # joblib.dump(clf, 'NB_spam_model.pkl')
-    # NB_spam_model = open('NB_spam_model.pkl','rb')
-    # clf = joblib.load(NB_spam_model)
+    #joblib.dump(clf, 'NB_spam_model.pkl')
+    #NB_spam_model = open('NB_spam_model.pkl', 'rb')
+    #clf = joblib.load(NB_spam_model)
 
     if request.method == 'POST':
         message = request.form['message']
         data = [message]
         vect = cv.transform(data).toarray()
-        my_prediction = nb.predict(vect)
+        my_prediction = clf.predict(vect)
     return render_template('result.html', prediction=my_prediction)
 
 
